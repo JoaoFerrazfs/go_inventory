0f4ed32761edddfda93d260a6f7174e7e8820994
JoaoFerrazfs <joaoferrazp@gmail.com>
2025-09-20 14:51:49 -0300

    refactor: remove air

--- SupplyInventory/Infrastructure/Db/MySqlConnection.go
+++ SupplyInventory/Infrastructure/Db/MySqlConnection.go
@@ -1,14 +1,34 @@
 package db
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() *gorm.DB {
-	dsn := "root:root@tcp(db:3306)/inventory?charset=utf8mb4&parseTime=True&loc=Local"
+	var dsn string
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+
+	fmt.Println("Conectando ao banco de dados MySQL...", dbHost, dbPort, dbName, dbUser)
+
+	// Se todas as variáveis estiverem definidas, monta o DSN
+	if dbHost != "" && dbPort != "" && dbName != "" && dbUser != "" {
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			dbUser, dbPass, dbHost, dbPort, dbName)
+	} else {
+		// fallback local padrão
+		dsn = "root:root@tcp(db:3306)/inventory?charset=utf8mb4&parseTime=True&loc=Local"
+	}
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Falha ao conectar no banco:", err)
@@ -19,7 +39,6 @@ func Connect() *gorm.DB {
 		log.Fatal(err)
 	}
 
-	// Testa a conexão
 	if err := sqlDB.Ping(); err != nil {
 		log.Fatal("Erro ao pingar o banco:", err)
 	}
